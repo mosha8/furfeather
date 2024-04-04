8fbb5c8f0d9e6f6e8cfe869cc60241f821670ba2
Moe <mohsen.r.shahbakhsh@gmail.com>
2024-04-04 09:49:45 +0330

    refactor: update button style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,6 +7,7 @@ const Button: FC<ButtonProps> = ({
   variant = 'contained',
   color = 'primary',
   size = 'medium',
+  className,
   href,
   externalUrl,
   target = '_self',
@@ -24,32 +25,38 @@ const Button: FC<ButtonProps> = ({
   const Component = href ? (externalUrl ? 'a' : Link) : 'button';
 
   return (
-    <div className={classnames(disabled && '!cursor-not-allowed')}>
-      <Component
-        className={classnames(
+    <Component
+      className={classnames(
+        'rounded-md',
+        isSizeSmall && 'px-2 py-1',
+        isSizeMedium && 'px-4 py-2',
+        (isVariantContained || isVariantOutlined) &&
           'inline-flex items-center justify-center',
-          'rounded-md',
-          isSizeSmall && 'px-2 py-1',
-          isSizeMedium && 'px-4 py-2',
-          isVariantContained && [
-            'text-sm leading-4 border-2 border-solid border-transparent',
-            isColorPrimary && 'bg-primary',
-          ],
-          isVariantOutlined && [
-            'text-sm leading-4 border-2 border-solid text-primary',
-            isColorPrimary && 'border-light',
-          ],
-          isVariantText && [isColorPrimary && 'text-primary hover:underline']
-        )}
-        disabled={disabled}
-        href={href as string}
-        target={target}
-        // @ts-expect-error @typescript-eslint/ban-ts-comment
-        onClick={onClick}
-      >
-        {children}
-      </Component>
-    </div>
+        isVariantContained && [
+          'text-sm leading-4 border-2 border-solid border-transparent',
+          isColorPrimary && 'bg-primary',
+        ],
+        isVariantOutlined && [
+          'text-sm leading-4 border-2 border-solid text-primary',
+          isColorPrimary && 'border-light',
+        ],
+        isVariantText && [
+          'relative',
+          isColorPrimary && 'text-primary',
+          isSizeMedium && 'link-underline-md',
+          isSizeSmall && 'link-underline-sm',
+        ],
+        disabled && ['cursor-not-allowed'],
+        className
+      )}
+      disabled={!!disabled}
+      href={href as string}
+      target={target}
+      // @ts-expect-error @typescript-eslint/ban-ts-comment
+      onClick={onClick}
+    >
+      {children}
+    </Component>
   );
 };
 
