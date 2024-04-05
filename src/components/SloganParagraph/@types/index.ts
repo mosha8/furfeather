689dc86fcd76b6c2dda51b989376045f7fcbad5e
Moe <mohsen.r.shahbakhsh@gmail.com>
2024-04-05 14:00:15 +0330

    refactor: improvements

--- src/components/SloganParagraph/@types/index.ts
+++ src/components/SloganParagraph/@types/index.ts
@@ -1,7 +1,6 @@
 import { HTMLAttributes } from 'react';
 
-export type SloganSize = 'medium' | 'large';
 export interface SloganParagraphProps
   extends HTMLAttributes<HTMLParagraphElement> {
-  size: SloganSize;
+  isFooterSlogan?: boolean;
 }
--- src/components/SloganParagraph/index.tsx
+++ src/components/SloganParagraph/index.tsx
@@ -1,18 +1,17 @@
 import { FC } from 'react';
-import { SloganParagraphProps } from './@types';
 import classNames from 'classnames';
+import { SloganParagraphProps } from './@types';
 
-const SloganParagraph: FC<SloganParagraphProps> = ({ size = 'medium' }) => {
-  // Variables
-  const isSizeMedium = size === 'medium';
-  const isSizeLarge = size === 'large';
+const SloganParagraph: FC<SloganParagraphProps> = ({
+  isFooterSlogan = false,
+}) => {
   return (
     <p
       className={classNames(
-        'hidden md:flex md:flex-col',
-        'text-primary leading-relaxed',
-        isSizeMedium && 'mx-0 my-3 text-3xl font-semibold',
-        isSizeLarge && 'mx-20 my-8 text-7xl font-bold'
+        'text-primary',
+        isFooterSlogan
+          ? 'hidden md:flex md:flex-col text-3xl font-semibold leading-relaxed'
+          : 'font-bold text-3xl md:text-7xl md:leading-relaxed'
       )}
     >
       Think, Design.
