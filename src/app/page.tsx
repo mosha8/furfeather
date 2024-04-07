99439c3476459d67c12a1d613f795943c491d6ed
Moe <mohsen.r.shahbakhsh@gmail.com>
2024-04-07 11:06:20 +0330

    feat: add home page first section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
+import OverlayLink from '@components/OverlayLink';
 import Tagline from '@components/Tagline';
+import classNames from 'classnames';
+import config from '@configs/app.json';
 
+const { BlogPostsImageUrls } = config;
 const Home = () => {
   return (
-    <div className="text-primary h-[1000px] py-4 mx-8 md:mx-16 lg:mx-20 my-8 md:my-12">
+    <div
+      className={classNames(
+        'text-primary py-4 mx-8 my-8',
+        'w-full flex flex-col gap-y-16',
+        'md:my-12 md:mx-16',
+        'lg:mx-20 lg:gap-y-24'
+      )}
+    >
       <Tagline />
+      <section
+        className={classNames(
+          'w-full flex flex-col items-start gap-y-6',
+          'md:flex-row md:gap-x-12 md:flex-wrap'
+        )}
+      >
+        {BlogPostsImageUrls.map(({ href, title, description, icon }) => (
+          <OverlayLink
+            key={`blogPost-overlayLink-${href}`}
+            href={href}
+            title={title}
+            description={description}
+            icon={icon}
+          />
+        ))}
+      </section>
     </div>
   );
 };
