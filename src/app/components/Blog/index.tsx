e451306353fd459128df9e37db1f2b58a51a9c14
Moe <mohsen.r.shahbakhsh@gmail.com>
2024-04-11 14:42:41 +0330

    chore: initial blog section

---
+++ src/app/components/Blog/index.tsx
@@ -0,0 +1,10 @@
+const Blog = () => {
+  return (
+    <div>
+      <h5 className="text-xl font-medium lg:text-3xl leading-relaxed">
+        Blog. Our very own meggazine.
+      </h5>
+    </div>
+  );
+};
+export default Blog;
