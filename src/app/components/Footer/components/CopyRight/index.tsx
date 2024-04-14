d0d6b8994783ce125ffc330ae16913f529ccc49c
Moe <mohsen.r.shahbakhsh@gmail.com>
2024-04-14 10:24:17 +0330

    refactor: link component enhancements

--- src/app/components/Footer/components/CopyRight/index.tsx
+++ src/app/components/Footer/components/CopyRight/index.tsx
@@ -1,11 +1,15 @@
 import Link from '@components/Link';
+import { BugAntIcon } from '@heroicons/react/24/solid';
 
 const CopyRight = () => {
   return (
     <div className="flex justify-between items-center text-sm text-light">
-      <span className="text-light">BugAnt</span>
+      <span className="text-light">
+        <BugAntIcon className="scale-75 hover:transition hover:duration-300 hover:delay-200 hover:rotate-90" />
+        BugAnt
+      </span>
       <Link variant="text" href="/" size="small" color="secondary">
-        Legal
+        Policy
       </Link>
     </div>
   );
